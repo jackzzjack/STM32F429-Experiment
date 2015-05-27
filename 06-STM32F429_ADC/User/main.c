b4668759c29186a4014f4ff9201ee844077be616
Yu-Ting, Kao @ MAC <jackzzjack@gmail.com>
2015-05-27 23:52:25 +0800

    Changelogs: 新增 ADC 註解 (Continue ...), Main Function.

--- 06-STM32F429_ADC/User/main.c
+++ 06-STM32F429_ADC/User/main.c
@@ -19,6 +19,9 @@
 #include "tm_stm32f4_adc.h"
 #include <stdio.h>
 
+/*
+ * 	利用 USART 把 ADC 訊息印出來。
+ */
 int main(void) {
 	char str[15];
 	
@@ -28,22 +31,32 @@ int main(void) {
 	/* Initialize Delay library */
 	TM_DELAY_Init();
 	
+	// 初始化 USART，包含 PinsPack 和 Bandrate。
 	/* Initialize USART1, 115200baud, TX: PB6 */
 	TM_USART_Init(USART1, TM_USART_PinsPack_2, 115200);
 	
+	/*
+	 * 	ADC
+	 * 		ADC 有 3 組，每一組有 15 個 channel
+	 */
+	// 初始化 ADC
 	/* Initialize ADC1 on channel 0, this is pin PA0 */
 	TM_ADC_Init(ADC1, ADC_Channel_0);
 	
+	// 初始化 ADC
 	/* Initialize ADC1 on channel 3, this is pin PA3 */
 	TM_ADC_Init(ADC1, ADC_Channel_3);
 	
 	while (1) {
+		// Infinity Loops
+		// 每次藉由 USART rs232 印出 ADC 的內容。
 		/* 							Read ADC1 Channel0					Read ADC1 Channel3 */
 		sprintf(str, "%4d: %4d\n\r", TM_ADC_Read(ADC1, ADC_Channel_0), TM_ADC_Read(ADC1, ADC_Channel_3));
 		
 		/* Put to USART */
 		TM_USART_Puts(USART1, str);
 		
+		// 每次 Delay 一下，100 milliseconds = 100 * 10^(-3) = 0.1 seconds.
 		/* Little delay */
 		Delayms(100);
 	}
